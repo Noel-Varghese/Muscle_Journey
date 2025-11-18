c34c05a4653ee00f33a8c7b6e52182d4616d90f1
Noel <varghesenoel333@gmail.com>
2025-11-18 22:33:25 +0530

    man i created like and posting logic

--- Backend/app/routes/post_routes.py
+++ Backend/app/routes/post_routes.py
@@ -6,6 +6,8 @@ from app.models.user_model import User
 from app.database import get_session
 from app.utils.auth_utils import get_current_user
 from pydantic import BaseModel
+from app.models.like_model import Like
+
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
@@ -44,3 +46,38 @@ def delete_post(post_id: int, session: Session = Depends(get_session), user: Use
     session.delete(post)
     session.commit()
     return {"message": "Post deleted successfully"}
+
+@router.post("/{post_id}/like")
+def like_post(post_id: int, session: Session = Depends(get_session), user: User = Depends(get_current_user)):
+    # Check if already liked
+    existing_like = session.exec(
+        select(Like).where(Like.post_id == post_id, Like.user_id == user.id)
+    ).first()
+
+    if existing_like:
+        raise HTTPException(status_code=400, detail="Post is already liked")
+
+    like = Like(post_id=post_id, user_id=user.id)
+    session.add(like)
+    session.commit()
+    return {"message": "Post liked!"}
+
+
+@router.delete("/{post_id}/like")
+def unlike_post(post_id: int, session: Session = Depends(get_session), user: User = Depends(get_current_user)):
+    like = session.exec(
+        select(Like).where(Like.post_id == post_id, Like.user_id == user.id)
+    ).first()
+
+    if not like:
+        raise HTTPException(status_code=404, detail="Like not found")
+
+    session.delete(like)
+    session.commit()
+    return {"message": "Like removed"}
+
+
+@router.get("/{post_id}/likes/count")
+def get_post_likes(post_id: int, session: Session = Depends(get_session)):
+    likes = session.exec(select(Like).where(Like.post_id == post_id)).all()
+    return {"likes_count": len(likes)}
